fb25e109aa1a55cae53808ff54bde4c89e580fa8
RHAtUAT <39317306+RHAtUAT@users.noreply.github.com>
2019-09-25 16:37:20 -0700

    Update settings.ts
    Added comments

--- dist/settings.js
+++ dist/settings.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Will save all the options selected in the options menu.
+ * I'd like to read from a config file, just because
+ * I haven't played around with that yet and I feel like
+ * it'd be fun, but if there's a correct way we should
+ * do that instead, just to keep it more 'professional'.
+ */
 class Settings {
     getSettings() {
     }
